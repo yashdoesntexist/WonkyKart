bb158462ef1aeaea9846dec11985dbc68049798c
cnabbotsford-prog <cnabbotsford@gmail.com>
2026-09-17 18:29:11 -0700

    sffgfgfgfdf

--- WonkyKartGame/Assets/Scripts/PlayerInputManager.cs
+++ WonkyKartGame/Assets/Scripts/PlayerInputManager.cs
@@ -5,6 +5,7 @@ public class PlayerInputManager : MonoBehaviour
 {
     public InputActionAsset input;
     public int speed;
+    public int turnSpeed;
     public PlayerManager manager;
     private bool isMovementDebugging;
     private InputAction move;
@@ -20,22 +21,28 @@ public class PlayerInputManager : MonoBehaviour
         moveForward = input.FindAction("MoveForward");
         turn = input.FindAction("Turn");
         rb = GetComponent<Rigidbody>();
+        rb.maxLinearVelocity = 5;
     }
 
     private void Update()
     {
         if (moveForward.IsPressed() && !isMovementDebugging)
         {
-            rb.linearVelocity = transform.rotation.eulerAngles * speed;
+            Vector2 direction = move.ReadValue<Vector2>() * speed;
+            rb.AddRelativeForce(new Vector3(direction.x, rb.linearVelocity.y, direction.y));
         }
         if (move.IsPressed() && isMovementDebugging)
         {
-            Vector2 direction = move.ReadValue<Vector2>();
-            direction *= speed;
+            Vector2 direction = move.ReadValue<Vector2>() * speed;
             rb.linearVelocity = new Vector3(direction.x, rb.linearVelocity.y, direction.y);
         } else
         {
             rb.linearVelocity = new Vector3(0, rb.linearVelocity.y, 0);
+        }
+        if (turn.IsPressed() && moveForward.IsPressed())
+        {
+            float direction = turn.ReadValue<float>();
+            gameObject.transform.Rotate(0, (direction * gameObject.transform.rotation.y) + (turnSpeed * direction), 0);
         }
     }
 }
